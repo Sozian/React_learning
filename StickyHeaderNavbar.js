81e7925892f45698489f236a98e643c88c3b978e
Aditya Raj Gupta <119107358+Sozian@users.noreply.github.com>
2024-08-16 12:49:07 +0530

    Create StickyHeaderNavbar.js

---
+++ StickyHeaderNavbar.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './Navbar.css';
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+  
+
+  return (
+   
+       
+       <header className='sticky-header'>
+        <nav>
+           <ul>
+              <li>Home</li>
+              <li>Login</li>
+           </ul>
+        </nav>
+       
+      </header>
+    
+    
+  );
+}
+
+export default Navbar;
